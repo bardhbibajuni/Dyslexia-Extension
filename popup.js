0fecb15a7d6894252d8a3f612c04c2f97e742a49
bardhbibajuni <bardh.bibaj@student.uni-pr.edu>
2025-12-12 14:19:54 +0100

    Update popup.js
    defaulti- bg color- lidhja me html -sliderat e fontit

--- popup.js
+++ popup.js
@@ -1,1 +1,48 @@
+const DEFAULT_SETTINGS = {
+    enabled: false,
+    font: "Original Font",
+    letterSpacing: 0,
+    wordSpacing: 0,
+    bgEnabled: false,
+    bgColor: "#ffffff"
+};
 
+const BG_COLORS = {
+    White: "#ffffff",
+    Beige: "#fef3c7",
+    Grey: "#f3f3f3",
+    Blue: "#dbeafe"
+};
+const DEFAULT_SETTINGS = {
+    enabled: false,
+    font: "Original Font",
+    letterSpacing: 0,
+    wordSpacing: 0,
+    bgEnabled: false,
+    bgColor: "#ffffff"
+};
+
+const BG_COLORS = {
+    White: "#ffffff",
+    Beige: "#fef3c7",
+    Grey: "#f3f3f3",
+    Blue: "#dbeafe"
+};
+
+let currentSettings = { ...DEFAULT_SETTINGS };
+
+let toggleEl;
+let fontSelectEl;
+let letterBar;
+let wordBar;
+let letterCircle;
+let wordCircle;
+let fontPreviewText;
+let letterPreviewText;
+let wordPreviewText;
+let colorOptions;
+let resetBtn;
+let saveBtn;
+
+let letterSliderCtrl;
+let wordSliderCtrl;
